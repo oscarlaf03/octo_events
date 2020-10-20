b77645c34d5380ff7b149ffeae911ccdde026170
Oscar Ortiz <ortizg.oscar@gmail.com>
2020-10-20 18:19:29 -0300

    done

--- app/services/WebhookInterpreter.rb
+++ app/services/WebhookInterpreter.rb
@@ -33,21 +33,39 @@ class WebhookInterpreter
         @readable ? @body['repository']['id'] : nil
     end
 
+    def sender
+        s = Sender.find_by github_id: sender_github_id
+        if s
+            return s
+        else
+           s = Sender.new(github_id: sender_github_id, username: @body['sender']['login'])
+           s.save
+           return s
+        end
+    end
+
+    def repository
+        r = Repository.find_by github_id: sender_github_id
+        if r
+            return r
+        else
+            r =  Repository.new(github_id: repository_github_id,name:@body['repository']['name'], url:@body['repository']['url'])
+            r.save
+            return r
+        end
+    end
+
+
     def save_event
         if @readable
-            @sender = Sender.find_by github_id: sender_github_id || Sender.new(github_id: sender_github_id, username: @body['sender']['login'])
-            @repository = Repository.find_by github_id: repository_github_id || Repository.new(github_id: repository_github_id,name:@body['repository']['name'], url:@body['repository']['url'])
-            @sender.save
-            @repository.save
             if standard_event?
                 @event = Event.new(event_type:@event_type, action:@body['action'])
             else push_event?
                 @event = Event.new(event_type:@event_type, action:@event_type)
             end
-            @event.repository = @repository
-            @event.sender = @sender
+            @event.repository = repository
+            @event.sender = sender
             @event.save
-
         end
     end
 end
